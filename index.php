7fd3b01f81cc7ff274081552b5426da385958e7c
heyozramos+ca1 <heyozramos+ca1@gmail.com>
2019-08-06 18:36:13 -0400

    Adds minimal title and content

--- index.php
+++ index.php
@@ -1,2 +1,21 @@
-<?php get_header() ?>
-<?php get_footer() ?>
+<?php get_header();
+if (have_posts()):
+  while(have_posts()): the_post(); ?>
+
+    <article <?php post_class() ?>>
+      <div class="container">
+        <header class="entry-header">
+          <h1><?= the_title() ?></h1>
+        </header>
+        <section class="entry-content">
+          <?= the_content() ?>
+        </section>
+        <footer class="entry-footer">
+          <?php wp_link_pages() ?>
+        </footer>
+      </div>
+    </article>
+
+  <?php endwhile;
+endif;
+get_footer();
