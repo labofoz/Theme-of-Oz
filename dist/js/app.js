23f9708e597b3d1a3afe78a0e119c243e67318a1
Oz Ramos <heyozramos@gmail.com>
2019-08-06 17:29:14 -0700

    #2 Uses package number for cache busting

--- dist/js/app.js
+++ dist/js/app.js
@@ -128,7 +128,7 @@ parcelRequire = (function (modules, cache, entry, globalName) {
     });
   });
 })(jQuery);
-},{}],"../../../../../.nvm/versions/node/v8.12.0/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{}],"../../../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -156,7 +156,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "37021" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64268" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -331,5 +331,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["../../../../../.nvm/versions/node/v8.12.0/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","js/app.js"], null)
+},{}]},{},["../../../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","js/app.js"], null)
 //# sourceMappingURL=/js/app.js.map
--- functions.php
+++ functions.php
@@ -22,7 +22,8 @@ add_action('after_setup_theme', function () {
  * - style.css isn't actually 
  */
 add_action('wp_enqueue_scripts', function () {
+  $package = json_decode(file_get_contents(get_template_directory() . '/package.json'));
   wp_enqueue_script('jquery');
-  wp_enqueue_script('app', get_template_directory_uri() . '/dist/js/app.js', ['jquery']);
+  wp_enqueue_script('app', get_template_directory_uri() . '/dist/js/app.js', ['jquery'], $package->version);
   wp_enqueue_style('style', get_template_directory_uri() . '/dist/styles/main.css');
 });
